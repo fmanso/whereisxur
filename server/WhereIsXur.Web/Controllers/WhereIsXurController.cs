3547644091e70e20f1d5ac6ec029586698190851
Fernando Manso <brok3r@gmail.com>
2016-01-12 23:38:40 +0100

    Parging exotics. Test deploy.

--- server/WhereIsXur.Web/Controllers/WhereIsXurController.cs
+++ server/WhereIsXur.Web/Controllers/WhereIsXurController.cs
@@ -1,23 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+using Newtonsoft.Json;
 using WhereIsXur.Web.Services;
 
 namespace WhereIsXur.Web.Controllers
 {
     public class WhereIsXurController : ApiController
     {
-        // GET: WhereIsXur
+        // GET: WhereIsXur        
+        [Route("api/whereisxur")]
         public async Task<HttpResponseMessage> Get()
         {
-            var location = await SearchXur(DateTime.UtcNow);
+            var location = await SearchXur(new DateTime(2016, 1, 9));
 
             return new HttpResponseMessage()
             {
                 Content = new StringContent(location, Encoding.ASCII)
             };
+        }
+
+        [Route("api/whereisxur/items")]
+        public async Task<HttpResponseMessage> GetItems()
+        {
+            var data = await SearchItems(new DateTime(2016, 1, 9));
+
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent(data, Encoding.ASCII)
+            };
+        }
+
+        private static async Task<string> SearchItems(DateTime today)
+        {
+            var whereIsXur = new WhereIsXurService();
+            if (!whereIsXur.IsXurWorking(today))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                var post = await whereIsXur.SearchPost(today);
+                if (post == null)
+                {
+                    return string.Empty;
+                }
+
+                var exotics = whereIsXur.ParseExotics(post);
+
+                if (string.IsNullOrEmpty(exotics))
+                {
+                    return string.Empty;
+                }
+                
+                return exotics;
+            }
         }
 
         private static async Task<string> SearchXur(DateTime today)
--- server/WhereIsXur.Web/Services/WhereIsXurService.cs
+++ server/WhereIsXur.Web/Services/WhereIsXurService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -23,6 +24,26 @@ namespace WhereIsXur.Web.Services
             var regExp = new Regex(@"Location:\*\*\n\n(?<location>.*?)\n\n");
             var match = regExp.Match(body);
             return match.Groups["location"].Value;
+        }
+
+        /// <summary>
+        /// Parse the exotics from the Xur's Megathread
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public string ParseExotics(string body)
+        {
+            //**Exotic Gear: **  \n\n Name | Type | Stats | Cost |\n: ----:|:----:|:-----:|:----:|\n[No Backup Plans](http://db.destinytracker.com/items/3055446326-no-backup-plans)|Titan Gauntlets|30 Discipline // 27 Strength|13 SC\n[Knucklehead Radar](http://db.destinytracker.com/items/1520434777-knucklehead-radar)|Hunter Helmet|35 Discipline // 36 Strength|13 SC\n[Apotheosis Veil](http://db.destinytracker.com/items/1519376145-apotheosis-veil)|Warlock Helmet|33 Intellect // 35 Strength|13 SC\n[Dragon's Breath](http://db.destinytracker.com/items/2808364179-dragons-breath)|Rocket Launcher||23 SC\n
+            var regExp = new Regex(@"\n\[(?<name>.*?)\]\((?<url>.*?)\)\|(?<type>.*?)\|");
+            var matches = regExp.Matches(body);
+
+            var stringBuilder = new StringBuilder();
+            foreach (Match match in matches)
+            {
+                stringBuilder.Append($"{match.Groups["name"].Value}\n");
+            }
+
+            return stringBuilder.ToString();
         }
 
         /// <summary>
